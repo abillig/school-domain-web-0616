654a570be62f4d1c968f10d3b71ea16173a67459
abillig <avrambillig@gmail.com>
2016-06-09 18:53:50 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
+require 'pry'
+
 # code here!
+class School
+
+def initialize(school_name)
+  @roster = {}
+end
+
+def roster
+  @roster
+end
+
+def add_student(name, grade)
+  @roster[grade] ||= []
+  @roster[grade] << name 
+end
+
+def grade(grade)
+  @roster[grade]
+end
+
+def sort
+  roster.each_with_object({}) do |(grade, array), hash|  
+    hash[grade] = array.sort 
+  end
+end
+
+end
